e9e632955f8f2bfa18557145fd27907716001b48
chiyoung <cldud970@naver.com>
2022-11-19 16:12:47 +0900

    fix(게시글 에디어 UI 변경)

---
+++ src/components/Notice/Posts/styles.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const PostHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const New = styled.div`
+  margin-top: 15.5px;
+  cursor: pointer;
+`;
+
+export const Newest = styled.span`
+  margin-right: 13px;
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+export const NewestComment = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${(props) => props.theme.color.gray};
+`;
