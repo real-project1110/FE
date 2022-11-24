9d29bdd2a0034c2724d27b7b9f65cc6df712cac5
chiyoung <100222045+Returnmakdo@users.noreply.github.com>
2022-11-24 15:38:13 +0900

    fix( trial 만료 후 재로그인)

--- src/apis/scheduleApi.js
+++ src/apis/scheduleApi.js
@@ -3,25 +3,25 @@ import instance from "../shared/instance";
 /** 일정 생성 { id:groupId, body: {title, description, start, end, color} } */
 export const addSchedule = async (payload) => {
   console.log(payload);
-  const data = await instance.post(`schedules/${payload.id}`, payload.body);
+  const data = await instance.post(`groups/schedules/${payload.id}`, payload.body);
   return data;
 };
 
 /** 일정 수정 { id, body:{ title, description, start, end, color  } } */
 export const editSchedule = async (payload) => {
   console.log(payload);
-  const data = await instance.put(`schedules/${payload.id}`, payload.body);
+  const data = await instance.put(`groups/schedules/${payload.id}`, payload.body);
   return data;
 };
 
 /** 일정 삭제 ( id ) */
 export const removeSchedule = async (payload) => {
-  const data = await instance.delete(`schedules/${payload}`);
+  const data = await instance.delete(`groups/schedules/${payload}`);
   return data;
 };
 
 // /** 일정 조회  */
 export const readSchedule = async (payload) => {
-  const { data } = await instance.get(`schedules/${payload}`);
+  const { data } = await instance.get(`groups/schedules/${payload}`);
   return data.data;
 };
