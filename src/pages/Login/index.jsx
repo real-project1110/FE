b58ee5c89bfef5d6a57ecf0a2452c4dc0f024d83
chiyoung <100222045+Returnmakdo@users.noreply.github.com>
2022-12-07 20:41:04 +0900

    fix: 1차배포

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { useNavigate } from "react-router-dom";
@@ -27,9 +27,9 @@ import GoogleLoginImage from "../../assets/image/구글.png";
 import BigLogoSvg from "../../assets/svg/BigLogoSvg";
 import login from "../../assets/image/login.png";
 import GoogleLogin from "react-google-login";
-import { gapi } from "gapi-script";
-import axios from "axios";
-import { setAccessToken } from "../../shared/Cookie/Cookie";
+// import { gapi } from "gapi-script";
+// import axios from "axios";
+// import { setAccessToken } from "../../shared/Cookie/Cookie";
 
 const Login = () => {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
@@ -37,56 +37,55 @@ const Login = () => {
     window.location.href = KAKAO_AUTH_URL;
   };
 
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        scope: "",
-      });
-    };
-    gapi.load("client:auth2", initClient);
-  });
+  // useEffect(() => {
+  //   const initClient = () => {
+  //     gapi.client.init({
+  //       clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  //       scope: "",
+  //     });
+  //   };
+  //   gapi.load("client:auth2", initClient);
+  // });
 
-  const onSuccess = async (res) => {
-    console.log(res);
-    try {
-      const token = res.accessToken;
-      const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}auth/google`, res, {
-        headers: {
-          Authorization: token,
-        },
-      });
-      const {
-        status,
-        data: { accessToken, refreshToken, currentPage },
-      } = response;
-      if (status !== 200) return;
-      setAccessToken(accessToken);
-      localStorage.setItem("token", refreshToken);
+  // const onSuccess = async (res) => {
+  //   try {
+  //     const token = res.accessToken;
+  //     const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}auth/google`, res, {
+  //       headers: {
+  //         Authorization: token,
+  //       },
+  //     });
+  //     const {
+  //       status,
+  //       data: { accessToken, refreshToken, currentPage },
+  //     } = response;
+  //     if (status !== 200) return;
+  //     setAccessToken(accessToken);
+  //     localStorage.setItem("token", refreshToken);
 
-      if (currentPage) {
-        return window.location.replace(`/groups/${currentPage}`);
-      } else {
-        return window.location.replace("/main/write");
-      }
-    } catch (e) {
-      // window.location.replace("/login");
-      toast.error("이미 가입된 이메일 입니다.", {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  };
+  //     if (currentPage) {
+  //       return window.location.replace(`/groups/${currentPage}`);
+  //     } else {
+  //       return window.location.replace("/main/write");
+  //     }
+  //   } catch (e) {
+  //     // window.location.replace("/login");
+  //     toast.error("이미 가입된 이메일 입니다.", {
+  //       position: "top-center",
+  //       autoClose: 1000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //       theme: "light",
+  //     });
+  //   }
+  // };
 
-  const onFailure = (err) => {
-    console.log("에러", err);
-  };
+  // const onFailure = (err) => {
+  //   console.log("에러", err);
+  // };
 
   const navigate = useNavigate();
   return (
@@ -140,7 +139,7 @@ const Login = () => {
                 <img src={GoogleLoginImage} alt="kakaoLogin" width="32px" height="32px" />
               </div>
               <span>구글 로그인</span>
-              <GoogleLogin icon={false} onSuccess={onSuccess} onFailure={onFailure} clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} buttonText="" />
+              <GoogleLogin icon={false} clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} buttonText="" />
             </SocialItem>
           </SocialList>
         </ButtonContainer>
